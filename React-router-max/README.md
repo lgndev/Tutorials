c5cc06a05c886b683946db19090054776fc0c1ee
lgndev <logan.sawyer.dev@gmail.com>
2023-07-29 13:55:44 -0400

    initial setup

---
+++ React-router-max/README.md
@@ -0,0 +1,1 @@
+Starter code to build out React Challenges
---
+++ React-router-max/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>Starter Code Base!</div>
+    </>
+  );
+}
+
+export default App;
